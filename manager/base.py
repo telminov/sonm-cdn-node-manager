acffa6304176e4faeb2c88b6e54c623d215b6aca
sergey@telminov.ru <sergey@telminov.ru>
2018-05-13 21:50:40 +0300

    fix #2 - инициализация сети с нодами

--- manager/base.py
+++ manager/base.py
@@ -3,6 +3,9 @@ from typing import List
 
 CND_NODE_IMAGE_NAME = 'redis:3.0.7'     # FIXME
 
+# простое деление по континентам. Можно сделать лучше через координаты.
+REGIONS = ('AF', 'AN', 'AS', 'EU', 'NA', 'OC', 'SA', 'DEFAULT')
+
 
 class Task:
     def __init__(self, name: str, region: str, created: datetime.datetime):
@@ -21,3 +24,13 @@ class Manager:
 
     def stop(self, task: Task):
         raise NotImplementedError()
+
+    def init_regions(self) -> List[Task]:
+        """Инициализация сети с нодами"""
+        tasks = []
+
+        for region in REGIONS:
+            task = self.create(name='node', region=region)
+            tasks.append(task)
+
+        return tasks
